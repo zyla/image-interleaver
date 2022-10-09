368e806cf74c27bd33e653094c5f682e7e7b2aa1
Maciej Bielecki <zyla@prati.pl>
2022-10-09 07:03:55 +0200

    fmt

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use clap::Parser;
 use anyhow::*;
-use std::path::PathBuf;
+use clap::Parser;
+use image::{GenericImage, GenericImageView};
 use std::cmp::min;
-use image::{GenericImage,GenericImageView};
+use std::path::PathBuf;
 
 #[derive(clap::Parser, Debug)]
 struct Args {
@@ -19,7 +19,7 @@ fn open_image(filename: &PathBuf) -> anyhow::Result<image::DynamicImage> {
 
 fn draw_vertical_line(image: &mut image::RgbImage, x: u32, y1: u32, y2: u32) {
     for y in y1..=y2 {
-        image.put_pixel(x, y, image::Rgb([0,0,0]));
+        image.put_pixel(x, y, image::Rgb([0, 0, 0]));
     }
 }
 
@@ -29,11 +29,19 @@ fn main() -> anyhow::Result<()> {
     let image2 = open_image(&args.file2)?.to_rgb8();
 
     if image1.width() != image2.width() {
-        bail!("images must have the same width ({} != {})", image1.width(), image2.width());
+        bail!(
+            "images must have the same width ({} != {})",
+            image1.width(),
+            image2.width()
+        );
     }
 
     if image1.height() != image2.height() {
-        bail!("images must have the same height ({} != {})", image1.height(), image2.height());
+        bail!(
+            "images must have the same height ({} != {})",
+            image1.height(),
+            image2.height()
+        );
     }
 
     let segment_width = image1.width() / args.num_segments;
@@ -43,15 +51,38 @@ fn main() -> anyhow::Result<()> {
     let mut source_x = 0;
     while source_x < image1.width() {
         let this_segment_width = min(segment_width, image1.width() - source_x);
-        result.copy_from(&*image1.view(source_x, 0, this_segment_width, image1.height()), 2 * source_x, 0)?;
-        draw_vertical_line(&mut result, 2*source_x, 0, 10);
-        draw_vertical_line(&mut result, 2*source_x, image1.height()-11, image1.height()-1);
-        result.copy_from(&*image2.view(source_x, 0, this_segment_width, image1.height()), 2 * source_x + this_segment_width, 0)?;
-        draw_vertical_line(&mut result, 2*source_x+this_segment_width, 0, 10);
-        draw_vertical_line(&mut result, 2*source_x+this_segment_width, image1.height()-11, image1.height()-1);
+        result.copy_from(
+            &*image1.view(source_x, 0, this_segment_width, image1.height()),
+            2 * source_x,
+            0,
+        )?;
+        draw_vertical_line(&mut result, 2 * source_x, 0, 10);
+        draw_vertical_line(
+            &mut result,
+            2 * source_x,
+            image1.height() - 11,
+            image1.height() - 1,
+        );
+        result.copy_from(
+            &*image2.view(source_x, 0, this_segment_width, image1.height()),
+            2 * source_x + this_segment_width,
+            0,
+        )?;
+        draw_vertical_line(&mut result, 2 * source_x + this_segment_width, 0, 10);
+        draw_vertical_line(
+            &mut result,
+            2 * source_x + this_segment_width,
+            image1.height() - 11,
+            image1.height() - 1,
+        );
         source_x += this_segment_width;
-        draw_vertical_line(&mut result, 2*source_x-1, 0, 10);
-        draw_vertical_line(&mut result, 2*source_x-1, image1.height()-11, image1.height()-1);
+        draw_vertical_line(&mut result, 2 * source_x - 1, 0, 10);
+        draw_vertical_line(
+            &mut result,
+            2 * source_x - 1,
+            image1.height() - 11,
+            image1.height() - 1,
+        );
     }
 
     result.save("result.png")?;
